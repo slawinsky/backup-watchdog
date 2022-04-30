0daf2686b4b95da5b4010321a5b4d6520cfbab64
Jakub Slawinski <jakubslawinsky@gmail.com>
2022-04-30 22:31:52 +0200

    add initTimer function inside DriectoryWatch class

--- src/watchdog/DirectoryWatch.java
+++ src/watchdog/DirectoryWatch.java
@@ -2,17 +2,45 @@ package watchdog;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DirectoryWatch {
     private Path path;
+    private long interval;
+    private boolean isFileCreated;
 
     public DirectoryWatch(String path) throws IOException, InterruptedException {
         this.path = Path.of(path);
+        this.interval = 10000;
+        this.isFileCreated = false;
+        initTimer();
         startService();
     }
 
+    private void initTimer() {
+        System.out.println("start initTimer\n");
+        Timer timer = new Timer();
+        int begin = 0;
+        int interval = 10000;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                checkIsFileCreated();
+            }
+        }, begin, interval);
+    }
+
+    private void checkIsFileCreated() {
+        if(isFileCreated) {
+            System.out.println("Backup został utworzony!");
+        } else {
+            System.out.println("Backup nie został utworzony!");
+        }
+    }
+
     private void startService() throws IOException, InterruptedException {
+        System.out.println("start startService\n");
         WatchService watchService = FileSystems.getDefault().newWatchService();
         path.register(
                 watchService,
@@ -23,6 +51,7 @@ public class DirectoryWatch {
         while((key = watchService.take()) != null) {
             for(WatchEvent<?> event : key.pollEvents()) {
                 System.out.println("Event: " + event.kind() + ", wykorzystany plik: " + event.context() + ".");
+                isFileCreated = true;
             }
             key.reset();
         }
